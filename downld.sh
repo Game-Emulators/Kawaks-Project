d799ff81831711ddd3bfd6cf516c6277f419bf9a
yaowenxu <yaowen1996@hotmail.com>
2021-02-01 14:31:04 +0800

    add screanshots and support-files
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

--- downld.sh
+++ downld.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# refresh the files
+# basic files
 curl -OL http://cps2shock.emu-france.info/download/winkawaks165.zip
 curl -OL http://kawaks.retrogames.com/hires-skin.zip
 curl -OL http://kawaks.retrogames.com/sshots.zip
@@ -8,3 +8,18 @@ curl -OL http://kawaks.retrogames.com/sshots138.zip
 curl -OL http://kawaks.retrogames.com/sshots142.zip
 curl -OL https://www.winkawaks.org/kawaks/kaillera.zip
 curl -OL https://www.winkawaks.org/kawaks/WinKawaks.full.zip
+
+# screenshots
+curl -OL http://kawaks.retrogames.com/pics/maincap.gif
+curl -OL http://kawaks.retrogames.com/pics/cap1.gif
+curl -OL http://kawaks.retrogames.com/pics/cap2.gif
+curl -OL http://kawaks.retrogames.com/pics/cap3.gif
+curl -OL http://kawaks.retrogames.com/pics/cap4.gif
+curl -OL http://kawaks.retrogames.com/pics/cap5.gif
+curl -OL http://kawaks.retrogames.com/pics/cap6.gif
+
+# support files
+curl -OL http://kawaks.retrogames.com/mfc4261.zip
+curl -OL http://kawaks.retrogames.com/msvcp60.zip
+curl -OL http://kawaks.retrogames.com/W95ws2setup.exe
+curl -OL http://kawaks.retrogames.com/redump36.zip
