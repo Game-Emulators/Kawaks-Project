469370400c6845cc0a569835e003bf264ca88a10
yaowenxu <yaowen1996@hotmail.com>
2021-02-01 15:45:48 +0800

    add comments
    Signed-off-by: yaowenxu <yaowen1996@hotmail.com>

--- roms/process.py
+++ roms/process.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 print("rom-names-processing")
 
+# Yaowen Xu
+# 2021年2月1日
+
 # 读文件并输出到 STDOUT
 with open('rom-names.txt', 'r') as f:
     with open('rom-names-with-hyphen.txt', 'w') as of:
